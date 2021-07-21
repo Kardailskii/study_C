a5528789df63e06448a080234d2e3504c13abdeb
Kardailskii <D.kardailskii@gmail.com>
2021-07-21 18:49:15 +0300

    6 задачка

---
+++ tst_1.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include "cmocka\include\cmocka.h"
+
+
+static void test_1(void **state) {
+
+    // вот здесь (то есть ниже) вы можете делать все что захотите (в пределах синтаксиса языка Си)
+
+extern int qqz;
+extern int mg;
+mg=1234;
+assert_int_equal(mg,1234);
+    // все, заканчиваем... ниже ничего писать не надо
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test( test_1 ),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
